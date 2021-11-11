e346a84dbf463bd1e84940fea606caa948c60a71
Zaai90 <54512095+Zaai90@users.noreply.github.com>
2021-11-11 16:27:27 +0100

    Added Databas stuff

---
+++ GameLib/Database/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace GameLib
+{
+    public class Database
+    {
+
+        public Player GetCharacter()
+        {
+            Player ply;
+
+            return ply;
+        }
+    }
+}
