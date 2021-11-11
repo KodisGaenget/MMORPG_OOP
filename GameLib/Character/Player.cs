7a030ddf36b832540e3d85c906e7212d160795c9
Zaai90 <54512095+Zaai90@users.noreply.github.com>
2021-11-11 20:51:06 +0100

    added methods

--- GameLib/Character/Player.cs
+++ GameLib/Character/Player.cs
@@ -8,7 +8,7 @@ namespace GameLib
         private List<Item> inventory = new();
         private List<Item> equipped = new();
         public int CurrentExp { get; private set; }
-        public int Position { get; private set; }
+
         IClass charClass;
 
         public Player(IClass charClass)
@@ -16,29 +16,59 @@ namespace GameLib
             this.charClass = charClass;
         }
 
-        private Player()
-        {
-
-        }
+        private Player() { }
 
         public override int Attack()
         {
             throw new NotImplementedException();
         }
 
-        public void Equip()
+        public void Equip(Item item)
         {
-
+            equipped.Add(item);
         }
-        public void UnEquip()
+        public void UnEquip(Item item)
         {
+            equipped.Remove(item);
+            AddItemToInventory(item);
+        }
 
+        public void AddItemToInventory(Item item)
+        {
+            inventory.Add(item);
+        }
+
+        public void ChangeHealth(int value)
+        {
+            int newHp = CurrentHealth + value;
+
+            if (newHp >= OriginalHealth)
+            {
+                CurrentHealth = OriginalHealth;
+            }
+            else if (newHp < OriginalHealth)
+            {
+                CurrentHealth = newHp;
+            }
+            if (CurrentHealth <= 0)
+            {
+                System.Console.WriteLine("You are dead!");
+            }
+        }
+
+        public void ChangePosition(int newPos)
+        {
+            Position = newPos;
+        }
+
+        public void SaveCharacter(Database db)
+        {
+            db.SavePlayer(this);
         }
 
         public override string ToString()
         {
             return $"Id: {Id}, Name: {Name}, Original Hp: {OriginalHealth}, Current Hp: {CurrentHealth}, Power: {Power}, Armor: {Armor}, Damage: {Damage}, Level: {Level}, CurrentExp: {CurrentExp}";
         }
-
     }
 }
