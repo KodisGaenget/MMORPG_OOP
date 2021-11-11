18247ae884f31959b10b4fe7b341d8ed94f1e242
Zaai90 <54512095+Zaai90@users.noreply.github.com>
2021-11-11 19:44:19 +0100

    Added starting code

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using GameLib;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RoomHandler roomHandler = new();
+
+            List<Room> allRooms = new()
+            {
+                new Room(1, "Entrance", "You have entered a dark hallway. The door slams shut and you hear the lock turning behind you...\n\nYou lift your torch and see blablabla...\n\nWhat would you like to do?\n\n", new List<int> { 2 }),
+                new Room(2, "Room 2", "This is room 2", new List<int> { 1, 3 }),
+                new Room(3, "Room 3", "This is room 3", new List<int> { 2, 4, 5 }),
+                new Room(4, "Room 4", "This is room 4", new List<int> { 3 }),
+                new Room(5, "Room 5", "This is room 5", new List<int> { 4, 6 }),
+                new Room(6, "Room 6", "This is room 6", new List<int> { 5 })
+            };
+
+            roomHandler.AddRooms(allRooms);
+            Game game = new(roomHandler);
+            game.Start(game.LoadCharacter().Position);
+
         }
     }
 }
