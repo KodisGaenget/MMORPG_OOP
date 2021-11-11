60e5d14b080c697f387689e69a7fe6f3d2afd3e3
Zaai90 <54512095+Zaai90@users.noreply.github.com>
2021-11-11 19:06:40 +0100

    Added id and sets

--- GameLib/Character/Character.cs
+++ GameLib/Character/Character.cs
@@ -4,13 +4,14 @@ namespace GameLib
 {
     public abstract class Character
     {
+        internal int Id { get; init; }
         public string Name { get; init; }
-        public int OrignalHealth { get; init; }
+        public int OriginalHealth { get; init; }
         public int CurrentHealth { get; init; }
         public int Power { get; init; }
-        public int Armor { get; }
-        public int Damage { get; }
-        public int Level { get; }
+        public int Armor { get; internal set; }
+        public int Damage { get; internal set; }
+        public int Level { get; internal set; }
 
         public abstract int Attack();
 
