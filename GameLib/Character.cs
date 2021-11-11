5fe84facd8b6d2e3e91f7e9e0ffde06bf3c7d609
Zaai90 <54512095+Zaai90@users.noreply.github.com>
2021-11-11 11:20:47 +0100

    Added Character class

---
+++ GameLib/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GameLib
+{
+    public abstract class Character
+    {
+        public string Name { get; init; }
+        public int OrignalHealth { get; init; }
+        public int CurrentHealth { get; init; }
+        public int Power { get; init; }
+        public int Armor { get; }
+        public int Damage { get; }
+        public int Level { get; }
+
+        public abstract int Attack();
+
+    }
+
+}
