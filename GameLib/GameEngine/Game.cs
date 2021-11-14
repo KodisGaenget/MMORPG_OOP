e597983911392127611bf4a4b105e66d00f2acbb
Zaai90 <54512095+Zaai90@users.noreply.github.com>
2021-11-14 12:25:03 +0100

    added some testcode and comments

--- GameLib/GameEngine/Game.cs
+++ GameLib/GameEngine/Game.cs
@@ -16,28 +16,32 @@ namespace GameLib
             PlayerLoader playerLoader = new(db, 1);
             playerLoader.Load();
             player = playerLoader.GetCharacter();
-            Start(player.Position);
+            Start();
         }
 
-        public void Start(int _roomID)
+        public void Start()
         {
-            player.ChangeHealth(300);
-            // player.Inventory.AddItem(1, 1);
-            System.Console.WriteLine("Tryck enter för att spara");
-            // Console.ReadLine();
+            Console.WriteLine($"You are in {roomHandler.GetRoomName(player.Position)}");
 
-            PlayerSaver playerSaver = new(db, player);
-
+            // player.ChangeHealth(300); //Lägger till eller tar bort hp på players upp till maxhp och ner till 0
+            // player.Inventory.AddItem(1, 1); //Lägger till itemID: 1 i inventory
             // Console.WriteLine(player.ToString());
-            // System.Console.WriteLine(player.Name + " " + player.Equipment);
+            // System.Console.WriteLine(player.Name + " " + player.Equipment); //Skriver ut namn och nuvanade eq.
             // Console.Clear();
-            //Console.WriteLine($"You are in {roomHandler.GetRoomName(_roomID)}");
-            // ConsoleUtils.TypeWriter(roomHandler.DescribeRoom(1));
+
+            //ConsoleUtils.TypeWriter(roomHandler.DescribeRoom(player.Position));
+            System.Console.WriteLine(roomHandler.DescribeRoom(player.Position));
+            foreach (var room in roomHandler.GetConnectedRooms(player.Position))
+            {
+                Console.WriteLine(roomHandler.GetRoomName(room));
+            }
+
+            System.Console.WriteLine("Tryck Enter för att Spara och Avsluta");
+            Console.ReadLine();
+            PlayerSaver playerSaver = new(db, player); //När detta objektskapas, sparas players/inventory och eq.
+
+
             //Console.Write($"1. Examine room\n2. Move to ");
-            // foreach (var roomID in roomHandler.GetConnectedRooms(_roomID))
-            // {
-            //     Console.WriteLine(roomHandler.GetRoomName(roomID));
-            // }
         }
     }
 }
