4496287da26578b155c50f3b7304a2b0209ad9af
Zaai90 <54512095+Zaai90@users.noreply.github.com>
2021-11-14 13:54:30 +0100

    Added more testcode

--- GameLib/GameEngine/Game.cs
+++ GameLib/GameEngine/Game.cs
@@ -22,7 +22,7 @@ namespace GameLib
         public void Start()
         {
             Console.WriteLine($"You are in {roomHandler.GetRoomName(player.Position)}");
-
+            player.Inventory.RemoveItem(1);
             // player.ChangeHealth(300); //Lägger till eller tar bort hp på players upp till maxhp och ner till 0
             // player.Inventory.AddItem(1, 1); //Lägger till itemID: 1 i inventory
             // Console.WriteLine(player.ToString());
@@ -30,11 +30,11 @@ namespace GameLib
             // Console.Clear();
 
             //ConsoleUtils.TypeWriter(roomHandler.DescribeRoom(player.Position));
-            System.Console.WriteLine(roomHandler.DescribeRoom(player.Position));
-            foreach (var room in roomHandler.GetConnectedRooms(player.Position))
-            {
-                Console.WriteLine(roomHandler.GetRoomName(room));
-            }
+            // System.Console.WriteLine(roomHandler.DescribeRoom(player.Position));
+            // foreach (var room in roomHandler.GetConnectedRooms(player.Position))
+            // {
+            //     Console.WriteLine(roomHandler.GetRoomName(room));
+            // }
 
             System.Console.WriteLine("Tryck Enter för att Spara och Avsluta");
             Console.ReadLine();
