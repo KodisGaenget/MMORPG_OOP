23e6e108646d0af44583f939af509fd25e44211e
4l3x <alexander.auermaa@gmail.com>
2021-11-16 13:25:36 +0100

    CreateWeapon moved to it's own class

--- ItemAdder/MainMenu.cs
+++ ItemAdder/MainMenu.cs
@@ -29,61 +29,20 @@ namespace ItemAdder
             switch (selectedIndex)
             {
                 case 0:
-                    CreateWeapon();
+                    // Armor
                     break;
 
                 case 1:
-                    // CreateArmor();
+                    // Consumable
                     break;
 
                 case 2:
-                    // CreateConsumable();
+                    // Key
                     break;
                 case 3:
-                    // CreateKey();
+                    ItemAdder.CreateWeapon.Run();
                     break;
             }
-        }
-
-        public void CreateWeapon() // Flytta till egen klass med eller utan andra createfunktioner
-        {
-            Clear();
-            string name, type = "";
-            int minDmg, price, maxDmg;
-            string prompt = "Choose weapon type:";
-
-            List<string> options = new();
-
-            foreach (string item in Enum.GetNames(typeof(WeaponType)))
-            {
-                options.Add(item);
-            }
-
-            Menu menu = new Menu(prompt, options);
-            int selectedIndex = menu.GetMenuIndex();
-            for (int i = 0; i < options.Count; i++)
-            {
-                type = options[i];
-            }
-
-            Write($"\nYou chose to create a {type.ToLower()}.");
-            Write("\nSet name: ");
-            name = ReadLine();
-
-            Write("\nSet price: ");
-            price = Convert.ToInt32(ReadLine());
-
-            Write("\nSet minimum damage: ");
-            minDmg = Convert.ToInt32(ReadLine());
-
-            Write("\nSet maximum damage: ");
-            maxDmg = Convert.ToInt32(ReadLine());
-
-            WriteLine($"{type} created with the following stats: "); // Add details
-            builditem.AddWeapon(name, price, "Weapon", "Weapon", minDmg, maxDmg, type, 1);
-            WriteLine("Weapon added. Press any key to return to the menu...");
-            ReadKey();
-            Run();
         }
     }
 }
