ca548f0ef7a32441d0b5d0fd6f52d51672a974a7
igge <ilz85@protonmail.com>
2021-11-16 13:24:29 +0100

    commented out code that will be replaced

--- GameLib/Model/Room/RoomHandler.cs
+++ GameLib/Model/Room/RoomHandler.cs
@@ -12,9 +12,9 @@ namespace GameLib
         }
 
         public string ExamineRoom(int _roomID)
-        {            
+        {
             GetRoom(_roomID).RoomExamined = true;
-            return GetRoom(_roomID).ExamineRoom;
+            return GetRoom(_roomID).ExamineText;
         }
 
         public bool IsRoomExaminated(int _roomID)
@@ -63,68 +63,68 @@ namespace GameLib
             allRooms.Add(new Room(9, "Room 9", "Room description string", "Room Examine string"));
             allRooms.Add(new Room(10, "Room 10", "Room description string", "Room Examine string"));
             allRooms.Add(new Room(11, "Room 11", "Room description string", "Room Examine string"));
-            ConnectRooms();
+            // ConnectRooms();
         }
 
-        private void ConnectRooms()
-        {
-            foreach (Room room in allRooms)
-            {
-                if (room.ID == 1)
-                {
-                    room.North = allRooms[1];
-                }
-                if (room.ID == 2)
-                {
-                    room.North = allRooms[4];
-                    room.East = allRooms[3];
-                    room.South = allRooms[0];
-                    room.West = allRooms[2];
-                }
-                if (room.ID == 3)
-                {
-                    room.East = allRooms[1];
-                }
-                if (room.ID == 4)
-                {
-                    room.West = allRooms[1];
-                }
-                if (room.ID == 5)
-                {
-                    room.North = allRooms[5];
-                    room.South = allRooms[1];
-                }
-                if (room.ID == 6)
-                {
-                    room.North = allRooms[7];
-                    room.South = allRooms[4];
-                    room.West = allRooms[6];
-                }
-                if (room.ID == 7)
-                {
-                    room.East = allRooms[5];
-                }
-                if (room.ID == 8)
-                {
-                    room.North = allRooms[10];
-                    room.East = allRooms[8];
-                    room.South = allRooms[5];
-                }
-                if (room.ID == 9)
-                {
-                    room.East = allRooms[9];
-                    room.West = allRooms[7];
-                }
-                if (room.ID == 10)
-                {
-                    room.West = allRooms[8];
-                }
-                if (room.ID == 11)
-                {
-                    room.South = allRooms[7];
-                }
-            }
+        // private void ConnectRooms()
+        // {
+        //     foreach (Room room in allRooms)
+        //     {
+        //         if (room.ID == 1)
+        //         {
+        //             room.North = allRooms[1];
+        //         }
+        //         if (room.ID == 2)
+        //         {
+        //             room.North = allRooms[4];
+        //             room.East = allRooms[3];
+        //             room.South = allRooms[0];
+        //             room.West = allRooms[2];
+        //         }
+        //         if (room.ID == 3)
+        //         {
+        //             room.East = allRooms[1];
+        //         }
+        //         if (room.ID == 4)
+        //         {
+        //             room.West = allRooms[1];
+        //         }
+        //         if (room.ID == 5)
+        //         {
+        //             room.North = allRooms[5];
+        //             room.South = allRooms[1];
+        //         }
+        //         if (room.ID == 6)
+        //         {
+        //             room.North = allRooms[7];
+        //             room.South = allRooms[4];
+        //             room.West = allRooms[6];
+        //         }
+        //         if (room.ID == 7)
+        //         {
+        //             room.East = allRooms[5];
+        //         }
+        //         if (room.ID == 8)
+        //         {
+        //             room.North = allRooms[10];
+        //             room.East = allRooms[8];
+        //             room.South = allRooms[5];
+        //         }
+        //         if (room.ID == 9)
+        //         {
+        //             room.East = allRooms[9];
+        //             room.West = allRooms[7];
+        //         }
+        //         if (room.ID == 10)
+        //         {
+        //             room.West = allRooms[8];
+        //         }
+        //         if (room.ID == 11)
+        //         {
+        //             room.South = allRooms[7];
+        //         }
+        //     }
 
-        }
+        // }
     }
 }
