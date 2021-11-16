b03bd22bf8cc0e5b4d210d484cb82ae8302eacaa
4l3x <alexander.auermaa@gmail.com>
2021-11-16 13:25:52 +0100

    Weapon menu added to menuhandler

--- ItemAdder/ItemAdder.cs
+++ ItemAdder/ItemAdder.cs
@@ -6,10 +6,12 @@ namespace ItemAdder
     class ItemAdder
     {
         public MainMenu MainMenu;
+        public CreateWeapon CreateWeapon;
         
         public ItemAdder()
         {
             this.MainMenu = new MainMenu(this);
+            this.CreateWeapon = new CreateWeapon(this);
         }
 
         public void Start()
