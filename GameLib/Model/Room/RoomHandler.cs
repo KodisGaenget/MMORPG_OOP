0b4b7586c83b2aa9ea6dd7b678d4dbd84d6dd48b
igge <ilz85@protonmail.com>
2021-11-17 13:32:41 +0100

    Connecting rooms are now linked on room creation

--- GameLib/Model/Room/RoomHandler.cs
+++ GameLib/Model/Room/RoomHandler.cs
@@ -39,92 +39,29 @@ namespace GameLib
 
         public Room GetRoom(int _roomID)
         {
-            Room getRoom = new();
-            foreach (var item in allRooms)
+            foreach (var room in allRooms)
             {
-                if (item.ID == _roomID)
+                if (room.ID == _roomID)
                 {
-                    getRoom = item;
+                    return room;
                 }
             }
-            return getRoom;
+            return null;
         }
 
         private void CreateRooms()
         {
-            allRooms.Add(new Room(1, "Room 1", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(2, "Room 2", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(3, "Room 3", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(4, "Room 4", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(5, "Room 5", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(6, "Room 6", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(7, "Room 7", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(8, "Room 8", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(9, "Room 9", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(10, "Room 10", "Room description string", "Room Examine string"));
-            allRooms.Add(new Room(11, "Room 11", "Room description string", "Room Examine string"));
-            // ConnectRooms();
+            allRooms.Add(new Room(1, "Room 1", "Room description string", "Room Examine string", 2, null, null, null));
+            allRooms.Add(new Room(2, "Room 2", "Room description string", "Room Examine string", 5, 4, 1, 3));
+            allRooms.Add(new Room(3, "Room 3", "Room description string", "Room Examine string", null, 2, null, null));
+            allRooms.Add(new Room(4, "Room 4", "Room description string", "Room Examine string", null, null, null, 2));
+            allRooms.Add(new Room(5, "Room 5", "Room description string", "Room Examine string", 6, null, 2, null));
+            allRooms.Add(new Room(6, "Room 6", "Room description string", "Room Examine string", 8, null, 5, 7));
+            allRooms.Add(new Room(7, "Room 7", "Room description string", "Room Examine string", null, 6, null, null));
+            allRooms.Add(new Room(8, "Room 8", "Room description string", "Room Examine string", 11, 9, 6, null));
+            allRooms.Add(new Room(9, "Room 9", "Room description string", "Room Examine string", null, 10, null, 8));
+            allRooms.Add(new Room(10, "Room 10", "Room description string", "Room Examine string", null, null, null, 9));
+            allRooms.Add(new Room(11, "Room 11", "Room description string", "Room Examine string", null, null, 8, null));
         }
-
-        // private void ConnectRooms()
-        // {
-        //     foreach (Room room in allRooms)
-        //     {
-        //         if (room.ID == 1)
-        //         {
-        //             room.North = allRooms[1];
-        //         }
-        //         if (room.ID == 2)
-        //         {
-        //             room.North = allRooms[4];
-        //             room.East = allRooms[3];
-        //             room.South = allRooms[0];
-        //             room.West = allRooms[2];
-        //         }
-        //         if (room.ID == 3)
-        //         {
-        //             room.East = allRooms[1];
-        //         }
-        //         if (room.ID == 4)
-        //         {
-        //             room.West = allRooms[1];
-        //         }
-        //         if (room.ID == 5)
-        //         {
-        //             room.North = allRooms[5];
-        //             room.South = allRooms[1];
-        //         }
-        //         if (room.ID == 6)
-        //         {
-        //             room.North = allRooms[7];
-        //             room.South = allRooms[4];
-        //             room.West = allRooms[6];
-        //         }
-        //         if (room.ID == 7)
-        //         {
-        //             room.East = allRooms[5];
-        //         }
-        //         if (room.ID == 8)
-        //         {
-        //             room.North = allRooms[10];
-        //             room.East = allRooms[8];
-        //             room.South = allRooms[5];
-        //         }
-        //         if (room.ID == 9)
-        //         {
-        //             room.East = allRooms[9];
-        //             room.West = allRooms[7];
-        //         }
-        //         if (room.ID == 10)
-        //         {
-        //             room.West = allRooms[8];
-        //         }
-        //         if (room.ID == 11)
-        //         {
-        //             room.South = allRooms[7];
-        //         }
-        //     }
-
-        // }
     }
 }
