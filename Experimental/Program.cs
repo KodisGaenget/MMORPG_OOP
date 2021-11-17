9350516ac05c79ee4075f3a5716f490759a44b7d
LucasB <lucas_benjaminson@hotmail.com>
2021-11-17 11:27:17 +0100

    Added experimental to try stuff out

---
+++ Experimental/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Characters;
+
+namespace Experimental
+{
+    class Program
+    {
+        private Player CurrentPlayer;
+        Enemy enemy = new();
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void DisplayInfo()
+        {
+            Console.BackgroundColor = Color;
+            Console.Write($"--- {Characters.Player.} ---");
+            Console.ResetColor();
+            Console.ForegroundColor = Color;
+            // Console.WriteLine($"\n{TextArt}\n");
+
+            //      public int Id { get; private init; }
+            // public string Name { get; private init; }
+            // public int OriginalHealth { get; protected set; }
+            // public int CurrentHealth { get; protected set; }
+            // public int Power { get; protected set; }
+            // public int Armor { get; protected set; }
+            // public int Damage { get; protected set; }
+            // public int Level { get; protected set; }
+        }
+        public void run()
+        {
+            CurrentPlayer = new Player();
+        }
+    }
+}
