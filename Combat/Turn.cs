954d698b28fb04ff650c4c2df917d8dbbb84792e
Zaai90 <erik.grune@gmail.com>
2021-11-18 14:06:31 +0100

    Turn can now attack and get back a result

--- Combat/Turn.cs
+++ Combat/Turn.cs
@@ -9,31 +9,32 @@ namespace Combat
 {
     public class Turn
     {
+        IFightable dealer;
+        IFightable taker;
+        int rawDmg = 0;
+        int resist = 0;
 
-        // private string Attack()
-        // {
-        //     int dealtDmg = 0;
-        //     string EndingMessage = "";
-        //     dealtDmg = CombatResult(CalcPlayerDmg() + player.Attack(), player, enemy, ArmorResist(player, enemy));
-        //     EndingMessage = CheckCombatOver();
-
-        //     if (EndingMessage != "")
-        //     {
-        //         return EndingMessage;
-        //     }
-        //     else
-        //     {
-        //         return $"You dealt {dealtDmg} to {fighter2.Name}.";
-        //     }
-
-        // }
-
-        private int CombatResult(int dmg, IFightable dealer, IFightable taker, int resist)
+        public Turn(IFightable dealer, IFightable taker, int rawDmg, int resistance)
         {
-            int realDmg = dmg * resist;
+            this.dealer = dealer;
+            this.taker = taker;
+            this.rawDmg = rawDmg;
+            resist = resistance;
+        }
+
+        public string Attack()
+        {
+            int dealtDmg = 0;
+            dealtDmg = CombatResult(rawDmg, resist);
+            return $"{dealer.Name} dealt {dealtDmg} to {taker.Name}.";
+
+        }
+
+        private int CombatResult(int dmg, int resist)
+        {
+            int realDmg = (dmg * resist);
             taker.ChangeHealth(realDmg);
             return realDmg;
-            //TODO Räkna ut hur mycket skada som görs på motståndaren.
         }
 
 
