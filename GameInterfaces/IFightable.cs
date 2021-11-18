bf4766f3b6633c8eafc072598aae77f5dd4a16ae
Zaai90 <erik.grune@gmail.com>
2021-11-18 14:07:15 +0100

    Added a few props in IFightable

--- GameInterfaces/IFightable.cs
+++ GameInterfaces/IFightable.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace GameInterfaces
 {
 
@@ -8,13 +10,17 @@ namespace GameInterfaces
         public int CurrentHealth { get; set; }
         public int Power { get; }
         public int Armor { get; }
+        public int Penetration { get; }
         public int Damage { get; }
         public int Level { get; }
-        public int Position { get; set; }
+        public int Position { get; }
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
 
         int Attack();
         int Block();
         bool ChangeHealth(int value);
+        public List<int> GetItemIdsFromEquipment();
     }
 
 
