90211adf9356d81d3b8ff7d9b85d2c3a6381b94d
igge <ilz85@protonmail.com>
2021-11-19 14:56:37 +0100

    fixed typo and nullable gets

--- GameLib/Model/Room/Room.cs
+++ GameLib/Model/Room/Room.cs
@@ -11,13 +11,13 @@ namespace GameLib
         public string ExamineText { get; set; }
 
         public bool RoomExamined { get; set; }
-        public int? North { get; set; }
-        public int? East { get; set; }
-        public int? South { get; set; }
-        public int? West { get; set; }
-        public int? ItemInRoomID { get; set; }
-        public int? ItemRequierdToEnter { get; set; }
-        public int? EnemyInRoom { get; set; }
+        public int? North { get { return North.GetValueOrDefault(); } set { North = value; } }
+        public int? East { get { return East.GetValueOrDefault(); } set { East = value; } }
+        public int? South { get { return South.GetValueOrDefault(); } set { South = value; } }
+        public int? West { get { return West.GetValueOrDefault(); } set { West = value; } }
+        public int? ItemInRoomId { get { return ItemInRoomId.GetValueOrDefault(); } set { ItemInRoomId = value; } }
+        public int? ItemRequiredToEnter { get { return ItemRequiredToEnter.GetValueOrDefault(); } set { ItemRequiredToEnter = value; } }
+        public int? EnemyInRoom { get { return EnemyInRoom.GetValueOrDefault(); } set { EnemyInRoom = value; } }
 
         public Room(int _Id, string _name, string _description, string _examineText, int? _north, int? _east, int? _south, int? _west)
         {
