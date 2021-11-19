00fd8714a20a455c4d10d33fbd0cc44ae9cfbf38
Zaai90 <erik.grune@gmail.com>
2021-11-19 15:48:01 +0100

    Fixed more errors

--- GameLib/Model/Room/Room.cs
+++ GameLib/Model/Room/Room.cs
@@ -12,13 +12,13 @@ namespace GameLib
         public string ExamineText { get; set; }
 
         public bool RoomExamined { get; set; }
-        public int? North { get { return North.GetValueOrDefault(); } set { } }
-        public int? East { get { return East.GetValueOrDefault(); } set { } }
-        public int? South { get { return South.GetValueOrDefault(); } set { } }
-        public int? West { get { return West.GetValueOrDefault(); } set { } }
-        public int? ItemInRoomId { get { return ItemInRoomId.GetValueOrDefault(); } set { } }
-        public int? ItemRequiredToEnter { get { return ItemRequiredToEnter.GetValueOrDefault(); } set { } }
-        public int? EnemyInRoom { get { return EnemyInRoom.GetValueOrDefault(); } set { } }
+        public int? North { get; set; }
+        public int? East { get; set; }
+        public int? South { get; set; }
+        public int? West { get; set; }
+        public int? ItemInRoomId { get; set; }
+        public int? ItemRequiredToEnter { get; set; }
+        public int? EnemyInRoom { get; set; }
 
         public Room(int _Id, string _name, string _description, string _examineText, int? _north, int? _east, int? _south, int? _west)
         {
