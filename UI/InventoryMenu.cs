e307254584e1b326adc3715cef7e740fe34e4830
igge <ilz85@protonmail.com>
2021-11-23 14:05:45 +0100

    Added InventoryMenu

---
+++ UI/InventoryMenu.cs
@@ -0,0 +1,83 @@
+using GameLib;
+using Items;
+using System;
+using System.Collections.Generic;
+
+namespace UI
+{
+    class InventoryMenu
+    {
+        List<InventoryInfo> inventory;
+        int selectedIndex;
+        Game game;
+
+        public InventoryMenu(List<InventoryInfo> _inventory, Game _game)
+        {
+            inventory = _inventory;
+            selectedIndex = 0;
+            game = _game;
+        }
+
+        public void Run()
+        {
+            ConsoleKeyInfo keyPressed;
+            do
+            {
+                Console.Clear();
+                DisplayInventory();
+                keyPressed = Console.ReadKey(true);
+
+                if (keyPressed.Key == ConsoleKey.DownArrow)
+                {
+                    selectedIndex++;
+                    if (selectedIndex == inventory.Count)
+                    {
+                        selectedIndex = 0;
+                    }
+                }
+
+                if (keyPressed.Key == ConsoleKey.UpArrow)
+                {
+                    selectedIndex--;
+                    if (selectedIndex == -1)
+                    {
+                        selectedIndex = inventory.Count - 1;
+                    }
+                }
+
+
+            } while (keyPressed.Key != ConsoleKey.Enter && keyPressed.Key != ConsoleKey.Escape);
+
+            if (keyPressed.Key == ConsoleKey.Enter)
+            {
+                UseItem();
+            }
+        }
+
+
+        private void DisplayInventory()
+        {
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                string inventoryItem = inventory[i].Name;
+                int itemAmount = inventory[i].Amount;
+                string prefix;
+
+                if (i == selectedIndex)
+                {
+                    prefix = ">";
+                }
+                else
+                {
+                    prefix = " ";
+                }
+                Console.WriteLine($"{prefix} {itemAmount}x {inventoryItem}");
+            }
+        }
+
+        private void UseItem()
+        {
+            game.ConsumeItem(inventory[selectedIndex].Id);
+        }
+    }
+}
